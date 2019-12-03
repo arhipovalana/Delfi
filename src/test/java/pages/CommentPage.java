a73c2bb6076b85ad083df9b6916986fc87732ca8
E`Lana Mickaellsky <lana.qa.2019@gmail.com>
2019-12-03 15:33:22 +0200

    Comment page

---
+++ src/test/java/pages/CommentPage.java
@@ -0,0 +1,14 @@
+package pages;
+public class CommentPage {
+
+    private BaseFunction baseFunc;
+
+    // константа
+    // константа
+    // константа
+
+    public CommentPage(BaseFunction baseFunc) {
+        this.baseFunc = baseFunc;
+    }
+
+}
