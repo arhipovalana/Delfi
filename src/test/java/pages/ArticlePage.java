5a3a28467e0b2c3525cd2617886bba7f417f7a97
E`Lana Mickaellsky <lana.qa.2019@gmail.com>
2019-12-03 15:30:40 +0200

    Article page

---
+++ src/test/java/pages/ArticlePage.java
@@ -0,0 +1,14 @@
+package pages;
+
+public class ArticlePage {
+
+    private BaseFunction baseFunc;
+
+    // константа
+    // константа
+    // константа
+
+    public ArticlePage(BaseFunction baseFunc) {
+        this.baseFunc = baseFunc;
+    }
+}
