e5312487b739d39ed4ba6e255380e75b74b2d9a7
E`Lana Mickaellsky <lana.qa.2019@gmail.com>
2019-12-04 13:08:58 +0200

    Home page

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -16,33 +16,44 @@ public class HomePage {
         this.baseFunc = baseFunc;
     }
 
-    private List<WebElement> articleList() {
+    private List<WebElement> getArticleList() {
         List<WebElement> articleBlockList = baseFunc.getElementList(HOME_PAGE_ARTICLES);
         Assertions.assertFalse(articleBlockList.isEmpty(), "There is no any article on Home Page");
         return articleBlockList;
     }
 
+//    public String getTitleText(int articleIndex) {
+//    //    if (!getArticleList().isEmpty()) ; // тут нужна другая проверка - есть ли элемент в элементе
+//        return getArticleList().get(articleIndex).findElement(HOME_PAGE_ARTICLE_TITLES).getText().trim();
+//    }
+
     public String getTitleText(int articleIndex) {
-        if (!articleList().isEmpty()) ;
-        return articleList().get(articleIndex).findElement(HOME_PAGE_ARTICLE_TITLES).getText().trim();
+        WebElement article = getArticleList().get(articleIndex); // тут получила элемент номер Х
+        article.findElements(HOME_PAGE_ARTICLE_TITLES); // тут ищу элемент в элементе Х
+
+
+        return getArticleList().get(articleIndex).findElement(HOME_PAGE_ARTICLE_TITLES).getText().trim();
     }
 
-//    public Integer getCommentCount(int articleIndex){
-//        if (!articleList().isEmpty()) {
-//            return baseFunc.removeBrackets(articleList().get(articleIndex).findElement(HOME_PAGE_ARTICLE_COMMENTS).getText());
+//    public Integer getCommentCount(int articleIndex) {
+//        if (!baseFunc.getElementList(HOME_PAGE_ARTICLE_COMMENTS).isEmpty()) {
+//            String commentCount = baseFunc.removeBrackets(getArticleList().get(articleIndex).findElements(HOME_PAGE_ARTICLE_COMMENTS).get(articleIndex).getText());
+//            return baseFunc.changeStringToInteger(commentCount);
 //        } else {
-//            Integer commentList = 0;
-//            return commentList;
+//            Integer commentCount = 0;
+//            return commentCount;
 //        }
+//    }
 
     public Integer getCommentCount(int articleIndex) {
-        if (!articleList().isEmpty()) ;
-        return baseFunc.removeBrackets(articleList().get(articleIndex).findElement(HOME_PAGE_ARTICLE_COMMENTS).getText());
+        //    if (!getArticleList().isEmpty()) ; // тут нужна другая проверка - есть ли элемент в элементе
+        String commentCount = baseFunc.removeBrackets(getArticleList().get(articleIndex).findElement(HOME_PAGE_ARTICLE_COMMENTS).getText());
+        return baseFunc.changeStringToInteger(commentCount);
     }
 
-    public WebElement openArticlePage(int articleIndex) {
-        WebElement element = articleList().get(articleIndex).findElement(HOME_PAGE_ARTICLE_TITLES);
-        return baseFunc.clickOnElement(element);
-    }
+//    public WebElement openArticlePage(int articleIndex) {
+//        WebElement element = articleList().get(articleIndex).findElement(HOME_PAGE_ARTICLE_TITLES);
+//        return baseFunc.clickOnElement(element);
+//    }
 
 }
