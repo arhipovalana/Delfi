9c27f330dcc8c91b9b1bc12b11daf4bbf4d0cbaf
E`Lana Mickaellsky <lana.qa.2019@gmail.com>
2020-01-13 15:30:36 +0200

    Test

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,11 +1,9 @@
 package pages;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
 import java.util.List;
 
 public class HomePage {
