7a574ce9fa3caa8ad282a3aa23c073070dd6aae2
E`Lana Mickaellsky <lana.qa.2019@gmail.com>
2020-01-13 15:15:29 +0200

    Test

--- src/test/java/pages/BaseFunction.java
+++ src/test/java/pages/BaseFunction.java
@@ -12,61 +12,40 @@ public class BaseFunction {
     private WebDriver browserDrv;
     private final Logger LOGGER = LogManager.getLogger(BaseFunction.class);
 
-    // создаем окно браузера - конструктор
     public BaseFunction() {
         System.setProperty("webdriver.chrome.driver", "c:/chromedriver.exe");
         browserDrv = new ChromeDriver();
         browserDrv.manage().window().maximize();
     }
 
-    // создаем набор функций для работы с созданным окном браузера
-
-    // функция, которая переходит по ссылке
     public void goToUrl(String url) {
-        if (url.startsWith("http://") || url.startsWith("https://")) {       // две палочки  || - это or
+        if (url.startsWith("http://") || url.startsWith("https://")) {
         } else {
             url = "http://" + url;
         }
         browserDrv.get(url);
     }
 
-    // функция, которая ищет первый элемент с помощью локатора
     public WebElement getElement(By locator) {
         return browserDrv.findElement(locator);
     }
 
-    // функция, которая ищет все элементы с помощью локатора и создаёт список
     public List<WebElement> getElementList(By locator) {
         return browserDrv.findElements(locator);
     }
 
-    //функция, которая получает String из элемента
-
-//    // функция, которая удаляет скобки и возвращает Integer
-//    public Integer removeBrackets(String valueWithBrackets){
-//        return Integer.valueOf(valueWithBrackets.replaceAll("([()])","").trim());
-//    }
-
-    // функция, которая удаляет скобки из текста
     public String removeBrackets(String textWithBrackets){
         return textWithBrackets.replaceAll("([()])","").trim();
     }
-//    //  функция, которая удаляет скобки и цыфры из текста
-//    public String removeBracketsAndNumbers(String textWithBracketsAndNumbers){
-//        return textWithBracketsAndNumbers.replaceAll("([(0-9)])","").trim();
-//    }
 
-    // функция, которая меняет тип String на Integer
     public Integer changeStringToInteger(String text){
         return Integer.valueOf(text);
     }
 
-    // функция, которая кликает по элементу
     public void clickOnElement(WebElement element) {
         element.click();
     }
 
-    // функция, которая закрывает браузер
     public void quit(){
         browserDrv.close();
     }
