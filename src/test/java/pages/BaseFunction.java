c3554d17809fd3d15834a8059e50638246bb7782
E`Lana Mickaellsky <lana.qa.2019@gmail.com>
2020-01-13 15:24:06 +0200

    Test

--- src/test/java/pages/BaseFunction.java
+++ src/test/java/pages/BaseFunction.java
@@ -5,17 +5,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class BaseFunction {
 
     private WebDriver browserDrv;
+    private WebDriverWait wait;
     private final Logger LOGGER = LogManager.getLogger(BaseFunction.class);
 
     public BaseFunction() {
         System.setProperty("webdriver.chrome.driver", "c:/chromedriver.exe");
         browserDrv = new ChromeDriver();
         browserDrv.manage().window().maximize();
+        browserDrv.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(browserDrv,10,500);
     }
 
     public void goToUrl(String url) {
